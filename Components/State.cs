4f4cff6ca25685dd2a2314158414565ba9e3ebe7
txiribimakula <pablotobalina@gmail.com>
2024-06-22 21:07:52 +0200

    basic state

---
+++ Components/State.cs
@@ -0,0 +1,13 @@
+public class State
+{
+    public State()
+    {
+        Partitions = new List<Partition>();
+    }
+
+    public List<Partition> Partitions { get; set; }
+}
+
+public class Partition
+{
+}
