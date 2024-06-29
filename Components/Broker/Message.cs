4b0a5725ee7177fb64735a48fae115e6d9cba3e1
txiribimakula <pablotobalina@gmail.com>
2024-06-29 21:07:31 +0200

    produce message with key

--- Components/Broker/Message.cs
+++ Components/Broker/Message.cs
@@ -1,10 +1,13 @@
 namespace BlazorApp.Model;
 
 public class Message {
-    public Message(string value)
+    public Message(string key, string value)
     {
+        Key = key;
         Value = value;
     }
+
+    public string Key { get; set; }
 
     public string Value { get; set; }
 }
