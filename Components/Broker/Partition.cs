06c4f9b331c857d3267a39e5f0c648e7b34e5985
txiribimakula <pablotobalina@gmail.com>
2024-06-29 18:01:21 +0200

    empty partitions by default

--- Components/Broker/Partition.cs
+++ Components/Broker/Partition.cs
@@ -3,10 +3,7 @@ namespace BlazorApp.Model;
 public class Partition {
     public Partition()
     {
-        Messages = new List<Message>() {
-            new Message("content 1"),
-            new Message("content 2")
-        };
+        Messages = new List<Message>();
     }
 
     public List<Message> Messages { get; set; }
