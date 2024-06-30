2fc2876767558c01e441d7bf1242307f066a28de
txiribimakula <pablotobalina@gmail.com>
2024-06-30 22:21:26 +0200

    log object

---
+++ Components/Log.cs
@@ -0,0 +1,17 @@
+﻿namespace BlazorApp.Components
+{
+    public class Log
+    {
+        public Log (LogLevel severity, string message)
+        {
+            Severity = severity;
+            Message = message;
+        }
+
+        public LogLevel Severity { get; set; }
+
+        public string Message { get; set; }
+
+        public override string ToString() => Message;
+    }
+}
